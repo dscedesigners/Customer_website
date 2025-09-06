1161d2e04483945903b7ee66e96c57d3381897cb
wittyvishnu <vishnuwitty@gmail.com>
2025-09-06 18:50:15 +0530

    added productListing

--- src/axios.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_BACKEND_URL || "http://localhost:5000"}/api`,
-  withCredentials: true,
-});
